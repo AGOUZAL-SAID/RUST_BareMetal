d304230697405e34bd749325a63b011737f5cca0
sagouzal-23 <said.agouzal@telecom-paris.fr>
2025-02-19 18:36:28 +0300

    iterative implimentation of fibo

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -1,6 +1,13 @@
 fn fibo(n: u32) -> u32 {
+    let mut a: [u32; 3] = [0,1,0];
     if (n==0) {return 0}
     if (n==1) {return 1}
+    for _ in 0..=n-2{
+        a[2] = a[1] + a[0];
+        a[0] = a[1];
+        a[1] = a[2];
+    }
+    a[2]
     
 }
 
