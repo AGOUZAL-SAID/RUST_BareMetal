15f7a989dc9ddb4795d9e161cbb87c89e021e18a
sagouzal-23 <said.agouzal@telecom-paris.fr>
2025-03-05 09:50:45 +0100

    Displaying Only Correct Values part done

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -1,21 +1,27 @@
-fn fibo(n: u32) -> u32 {
+fn fibo(n: u32) -> Option<u32> {
     let mut a: [u32; 3] = [0, 1, 0];
     if n == 0 {
-        return 0;
+        return Some(0);
     }
     if n == 1 {
-        return 1;
+        return Some(1);
     }
     for _ in 0..=n - 2 {
-        a[2] = a[1].checked_add(a[0]).unwrap();
+        a[2] = a[1].checked_add(a[0])?;
         a[0] = a[1];
         a[1] = a[2];
     }
-    a[2]
+    Some(a[2])
 }
 
 fn main() {
     for i in 0..=50 {
-        println!("fibo({i})={}", fibo(i));
+        match fibo(i) {
+            Some(result) => println!("fibo({i}) = {result}"),
+            None => {
+                println!("fibo({i}) => u32 overflow");
+                break;
+            }
+        }
     }
 }
