8bf697634e625e93c297674264655072df3713a4
sagouzal-23 <said.agouzal@telecom-paris.fr>
2025-03-05 09:26:25 +0100

    Saturated Arithmetic part done

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -1,19 +1,21 @@
 fn fibo(n: u32) -> u32 {
-    let mut a: [u32; 3] = [0,1,0];
-    if (n==0) {return 0}
-    if (n==1) {return 1}
-    for _ in 0..=n-2{
-        a[2] = a[1].saturating_add(a[0]) ;
+    let mut a: [u32; 3] = [0, 1, 0];
+    if n == 0 {
+        return 0;
+    }
+    if n == 1 {
+        return 1;
+    }
+    for _ in 0..=n - 2 {
+        a[2] = a[1].saturating_add(a[0]);
         a[0] = a[1];
         a[1] = a[2];
     }
     a[2]
-    
 }
 
-
 fn main() {
-    for i in 0..=50{
-        println!("fibo({i})={}",fibo(i));
+    for i in 0..=50 {
+        println!("fibo({i})={}", fibo(i));
     }
 }
