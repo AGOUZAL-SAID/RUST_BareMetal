ddebe3c323329cb31fb9a048c9208a0a54c6154e
sagouzal-23 <said.agouzal@telecom-paris.fr>
2025-03-05 11:18:33 +0100

    Use of Crates part done

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -1,3 +1,5 @@
+use clap::Parser;
+
 fn fibo(n: u32) -> Option<u32> {
     let mut a: [u32; 3] = [0, 1, 0];
     if n == 0 {
@@ -14,10 +16,33 @@ fn fibo(n: u32) -> Option<u32> {
     Some(a[2])
 }
 
+/// compute fibonacci suite value
+#[derive(Parser)]
+struct Args {
+    /// The maximal number to print the fibo value of
+    value: Option<u32>,
+
+    /// Print intermediate values
+    #[clap(short, long, default_value_t = false)]
+    verbose: bool,
+
+    /// The minimum number to compute
+    #[clap(short, long)]
+    mini: Option<u32>,
+}
 fn main() {
-    for i in 0..=50 {
+    let args = Args::parse();
+    let min: u32 = args.mini.unwrap_or(0);
+    let max: u32 = args.value.unwrap_or(50); // Par défaut, on affiche jusqu'à 50
+    let all: bool = args.verbose;
+
+    for i in min..=max {
         match fibo(i) {
-            Some(result) => println!("fibo({i}) = {result}"),
+            Some(result) => {
+                if all || i == max {
+                    println!("fibo({i}) = {result}");
+                }
+            }
             None => {
                 println!("fibo({i}) => u32 overflow");
                 break;
