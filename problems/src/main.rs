14c80c609115b89e96fe2909fe0596d49bc22caa
sagouzal-23 <said.agouzal@telecom-paris.fr>
2025-03-12 19:52:03 +0100

    simple problems are done

--- problems/src/main.rs
+++ problems/src/main.rs
@@ -4,47 +4,68 @@ fn ret_string() -> String {
     String::from("  A String object  ")
 }
 
-fn choose_str(s1: & str, s2: & str, select_s1: bool) -> &str {
-    let a = s1 ;
-    let b = s2 ;
-    if select_s1 { a } else { b }
+fn choose_str<'c, 'a: 'c, 'b: 'c>(s1: &'a str, s2: &'b str, select_s1: bool) -> &'c str {
+    if select_s1 { s1 } else { s2 }
 }
 /// the error befor was because we use the value returned
 /// whiche the life time is immidiate so i give it to an vriable
 /// that will survive until the end of main
 fn main() {
-    let s = ret_string(); 
-    let word = s.trim(); 
+    let s = ret_string();
+    let word = s.trim();
+    let a: &str = "hello";
+    println!("{}", a);
     assert_eq!(word, "A String object");
-}
-enum OOR <'a> {
-    Owned(String),
-    Borrowed(&'a str), // `'static` is used for this example; you can customize lifetimes
-}
+    choose_str(a, word, false);
+    // Check Deref for both variants of OOR
+    let s1 = OOR::Owned(String::from("  Hello, world.  "));
+    assert_eq!(s1.trim(), "Hello, world.");
+    let mut s2 = OOR::Borrowed("  Hello, world!  ");
+    assert_eq!(s2.trim(), "Hello, world!");
 
-impl<'a> Deref for OOR<'a>  {
-    type Target = str;
-    fn deref(&self) -> &Self::Target {
+    // Check choose
+    let s = choose_str(&s1, &s2, true);
+    assert_eq!(s.trim(), "Hello, world.");
+    let s = choose_str(&s1, &s2, false);
+    assert_eq!(s.trim(), "Hello, world!");
 
-        match self {
-            OOR::Owned(word) => &word,               // Deref to the `String` stored in the `Owned` variant
-            OOR::Borrowed(word) => *word,            // Deref to the `&str` stored in the `Borrowed` variant
+    // Check DerefMut, a borrowed string should become owned
+    assert!(matches!(s1, OOR::Owned(_)));
+    assert!(matches!(s2, OOR::Borrowed(_)));
+    unsafe {
+        for c in s2.as_bytes_mut() {
+            if *c == b'!' {
+                *c = b'?';
+            }
         }
     }
-
-
+    assert!(matches!(s2, OOR::Owned(_)));
+    assert_eq!(s2.trim(), "Hello, world?");
+}
+enum OOR<'a> {
+    Owned(String),
+    Borrowed(&'a str),
 }
 
-impl<'a> DerefMut OOR<'a>{
-    fn deref_mut(&mut self) -> &mut Self::Target{
-        let mut a;
-        let mut b;
+impl Deref for OOR<'_> {
+    type Target = str;
+    fn deref(&self) -> &Self::Target {
         match self {
-            OOR:: Owned(word)=> {
-                a = word;
-                return & mut a},
-            OOR::Borrowed(word)=> {
-                b = *word;
-                return & mut b;
-            }
-        }}}
+            OOR::Owned(word) => word,
+            OOR::Borrowed(word) => word,
+        }
+    }
+}
+
+impl DerefMut for OOR<'_> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        if let OOR::Borrowed(s) = self {
+            *self = OOR::Owned(s.to_string().clone());
+        }
+        match self {
+            OOR::Owned(s) => s,
+            _ => unreachable!(),
+        }
+    }
+}
+//`word`
