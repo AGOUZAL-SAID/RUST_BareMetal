7e980e35f8aa0e53cfb5896873c8c98f4ca86c85
sagouzal-23 <said.agouzal@telecom-paris.fr>
2025-03-19 16:33:53 +0100

    Displaying something done successfully

--- tp-led-matrix/src/main.rs
+++ tp-led-matrix/src/main.rs
@@ -3,12 +3,12 @@
 
 use cortex_m_rt::entry;
 use embassy_stm32 as _;   // Just to link it in the executable (it provides the vector table)
+use panic_rtt_target as _;
+use rtt_target::{rprintln,rtt_init_print};
 
-#[panic_handler]
-fn panic_handler(_panic_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Hello, world!");
     panic!("The program stopped");
-}
+ }
