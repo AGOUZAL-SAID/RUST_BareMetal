a0fc1e41bd84d8fcf2930c568fe0ba7b577a411e
sagouzal-23 <said.agouzal@telecom-paris.fr>
2025-03-19 16:06:37 +0100

    Configuring the toolchain done

---
+++ tp-led-matrix/src/main.rs
@@ -0,0 +1,14 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use embassy_stm32 as _;   // Just to link it in the executable (it provides the vector table)
+
+#[panic_handler]
+fn panic_handler(_panic_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+#[entry]
+fn main() -> ! {
+    panic!("The program stopped");
+}
