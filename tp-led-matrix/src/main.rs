52002b1853814f9ce011afa2090fb4ad916557e7
sagouzal-23 <said.agouzal@telecom-paris.fr>
2025-03-20 12:55:58 +0100

    Configuring the hardware part done

--- tp-led-matrix/src/main.rs
+++ tp-led-matrix/src/main.rs
@@ -3,12 +3,31 @@
 
 use cortex_m_rt::entry;
 use embassy_stm32 as _;   // Just to link it in the executable (it provides the vector table)
-use panic_rtt_target as _;
-use rtt_target::{rprintln,rtt_init_print};
+use defmt_rtt as _;
+use panic_probe as _;
+use embassy_stm32::rcc::*;
+use embassy_stm32::Config;
+
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
-    rprintln!("Hello, world!");
-    panic!("The program stopped");
- }
+    defmt::info!("defmt correctly initialized");
+
+    // Setup the clocks at 80MHz using HSI (by default since HSE/MSI
+    // are not configured): HSI(16MHz)×10/2=80MHz. The flash wait
+    // states will be configured accordingly.
+    let mut config = Config::default();
+    config.rcc.hsi = true;
+    config.rcc.pll = Some(Pll {
+        source: PllSource::HSI,
+        prediv: PllPreDiv::DIV1,
+        mul: PllMul::MUL10,
+        divp: None,
+        divq: None,
+        divr: Some(PllRDiv::DIV2), // 16 * 10 / 2 = 80MHz
+    });
+    config.rcc.sys = Sysclk::PLL1_R;
+    embassy_stm32::init(config);
+
+    panic!("Everything configured");
+}
